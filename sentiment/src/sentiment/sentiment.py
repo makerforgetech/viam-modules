bf6840bb278c0f5f13ac02207cf4adf8b5505653
danic85 <dan.nicholson@outlook.com>
2023-12-09 19:46:06 +0000

    Updates, still failing

--- sentiment/src/sentiment/sentiment.py
+++ sentiment/src/sentiment/sentiment.py
@@ -3,16 +3,37 @@ import nltk
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 
-from typing import Final, ClassVar
+from typing import Final, ClassVar, Mapping
 from viam.services.service_base import ServiceBase
 from viam.resource.types import RESOURCE_TYPE_SERVICE, Subtype
 from viam.resource.types import Model
+
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model
+
+from viam import logging
+LOGGER = logging.getLogger(__name__)
+CACHEDIR = "/tmp/cache"
+
+import json
+
+from typing_extensions import Self
+
+
 
 
 class SentimentAnalysisModule(ServiceBase):
     
     SUBTYPE: Final = Subtype("makerforge", RESOURCE_TYPE_SERVICE, "sentiment")
     MODEL: ClassVar[Model] = Model.from_string("makerforge:viam-modules:sentiment")
+    
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+        sentiment = cls(config.name)
+        LOGGER.debug(json.dumps(sentiment.__dict__))
+        return sentiment
 
     def __init__(self):
         # Do this the first time
