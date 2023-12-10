dec48b1080c30a4a6635fd04083842e9b42d5c0b
danic85 <dan.nicholson@outlook.com>
2023-12-10 10:01:29 +0000

    Correct types

--- sentiment/src/__init__.py
+++ sentiment/src/__init__.py
@@ -2,9 +2,9 @@
 This file registers the model with the Python SDK.
 """
 
-from viam.services.vision import VisionClient
+from viam.services.mlmodel import MLModel
 from viam.resource.registry import Registry, ResourceCreatorRegistration
 
 from .sentiment import Sentiment
 
-Registry.register_resource_creator(VisionClient.SUBTYPE, Sentiment.MODEL, ResourceCreatorRegistration(Sentiment.new_sentiment, Sentiment.validate_config))
+Registry.register_resource_creator(MLModel.SUBTYPE, Sentiment.MODEL, ResourceCreatorRegistration(Sentiment.new_sentiment, Sentiment.validate_config))
--- sentiment/src/__main__.py
+++ sentiment/src/__main__.py
@@ -1,13 +1,13 @@
 import asyncio
 import sys
 
-from viam.services.vision import VisionClient
+from viam.services.mlmodel import MLModel
 from viam.module.module import Module
 from .sentiment import Sentiment
 
 async def main():
     module = Module.from_args()
-    module.add_model_from_registry(VisionClient.SUBTYPE, Sentiment.MODEL)
+    module.add_model_from_registry(MLModel.SUBTYPE, Sentiment.MODEL)
     await module.start()
 
 if __name__ == "__main__":
