885641a4c0fdbcc753c5398181eab0875de2fb57
danic85 <dan.nicholson@outlook.com>
2024-03-25 22:29:01 +0000

    Initial creation

---
+++ generic-serial/README.md
@@ -0,0 +1,65 @@
+# serial modular service
+
+This module implements the [rdk generic API](https://github.com/rdk/generic-api) in a makerforge:viam-modules:serial model.
+With this model, you can...
+
+## Requirements
+
+_Add instructions here for any requirements._
+
+``` bash
+```
+
+## Build and Run
+
+To use this module, follow these instructions to [add a module from the Viam Registry](https://docs.viam.com/registry/configure/#add-a-modular-resource-from-the-viam-registry) and select the `rdk:generic:makerforge:viam-modules:serial` model from the [`makerforge:viam-modules:serial` module](https://app.viam.com/module/rdk/makerforge:viam-modules:serial).
+
+## Configure your generic
+
+> [!NOTE]  
+> Before configuring your generic, you must [create a machine](https://docs.viam.com/manage/fleet/machines/#add-a-new-machine).
+
+Navigate to the **Config** tab of your robot’s page in [the Viam app](https://app.viam.com/).
+Click on the **Components** subtab and click **Create component**.
+Select the `generic` type, then select the `makerforge:viam-modules:serial` model. 
+Enter a name for your generic and click **Create**.
+
+On the new component panel, copy and paste the following attribute template into your generic’s **Attributes** box:
+
+```json
+{
+  TODO: INSERT SAMPLE ATTRIBUTES
+}
+```
+
+> [!NOTE]  
+> For more information, see [Configure a Robot](https://docs.viam.com/manage/configuration/).
+
+### Attributes
+
+The following attributes are available for `rdk:generic:makerforge:viam-modules:serial` generics:
+
+| Name | Type | Inclusion | Description |
+| ---- | ---- | --------- | ----------- |
+| `todo1` | string | **Required** |  TODO |
+| `todo2` | string | Optional |  TODO |
+
+### Example Configuration
+
+```json
+{
+  TODO: INSERT SAMPLE CONFIGURATION(S)
+}
+```
+
+### Next Steps
+
+_Add any additional information you want readers to know and direct them towards what to do next with this module._
+_For example:_ 
+
+- To test your...
+- To write code against your...
+
+## Troubleshooting
+
+_Add troubleshooting notes here._
---
+++ generic-serial/src/serial.py
@@ -0,0 +1,130 @@
+from typing import ClassVar, Mapping, Sequence, Any, Dict, Optional, Tuple, Final, List, cast
+from typing_extensions import Self
+from typing import Final
+
+from viam.resource.types import RESOURCE_NAMESPACE_RDK, RESOURCE_TYPE_SERVICE, Subtype
+
+from viam.module.types import Reconfigurable
+from viam.proto.app.robot import ComponentConfig
+from viam.proto.common import ResourceName, Vector3
+from viam.resource.base import ResourceBase
+from viam.resource.types import Model, ModelFamily
+
+from viam.services.generic import Generic
+from viam.logging import getLogger
+
+import time
+import asyncio
+
+from pubsub import pub
+from robust_serial import write_order, Order, write_i8, write_i16, read_i8, read_i16, read_i32, read_order
+from robust_serial.utils import open_serial_port
+
+LOGGER = getLogger(__name__)
+
+class serial(Generic, Reconfigurable):
+    
+    """
+    Generic service, which represents any type of service that can execute arbitrary commands
+    """
+    MODEL: ClassVar[Model] = Model(ModelFamily("makerforge", "viam-modules"), "serial")
+    
+    type_map=['led', 'servo', 'servo_relative', ' pin', 'read']
+    DEVICE_LED = 0
+    DEVICE_SERVO = 1
+    DEVICE_PIN = 2
+    DEVICE_PIN_READ = 3
+    DEVICE_SERVO_RELATIVE = 4
+    ORDER_RECEIVED = 5
+    
+    serial_file: Any
+    
+    # Constructor
+    @classmethod
+    def new(cls, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]) -> Self:
+        my_class = cls(config.name)
+        my_class.reconfigure(config, dependencies)
+        return my_class
+
+    # Validates JSON Configuration
+    @classmethod
+    def validate(cls, config: ComponentConfig):
+        return
+
+    # Handles attribute reconfiguration
+    def reconfigure(self, config: ComponentConfig, dependencies: Mapping[ResourceName, ResourceBase]):
+        # here we initialize the resource instance, the following is just an example and should be updated as needed
+        self.serial_file = open_serial_port(baudrate=115200, timeout=None)
+        pub.subscribe(self.send, 'serial')
+        return
+
+    """ Implement the methods the Viam RDK defines for the Generic API (rdk:service:generic) """
+
+    def read(self):
+        return read_i8(self.serial_file)
+
+    def read16(self):
+        return read_i16(self.serial_file)
+        
+    def send(self, type, identifier, message):
+        """
+        Examples:
+        # send(serial.DEVICE_SERVO, 18, 20)
+        # send(serial.DEVICE_LED, 1, (20,20,20))
+        # send(serial.DEVICE_LED, range(9), (20,20,20))
+        :param type: one of the DEVICE_ types
+        :param identifier: an identifier or list / range of identifiers, pin or LED number
+        :param message: the packet to send to the arduino
+        """
+        if self.serial_file is None:
+            return
+
+        pub.sendMessage('led', identifiers='status5', color='blue')
+        # print('[serial] ' + str(serial.type_map[type]) + ' id: ' + str(identifier) + ' val: ' + str(message))
+
+        pub.sendMessage('log', msg='[serial] ' + str(self.type_map[type]) + ' id: ' + str(identifier) + ' val: ' + str(message))
+        if type == serial.DEVICE_SERVO or type == 'servo':
+            write_order(self.serial_file, Order.SERVO)
+            write_i8(self.serial_file, identifier)
+            write_i16(self.serial_file, int(message))
+            pub.sendMessage('log', msg="[serial] Servo(relative) " + str(identifier) + " " + str(message))
+            # print('[serial] Moved value from Arduino: ' + str(self.read16()))
+            pub.sendMessage('log', msg='[serial] Moved value from Arduino: ' + str(self.read16()))
+        if type == serial.DEVICE_SERVO_RELATIVE or type == 'servo_relative':
+            write_order(self.serial_file, Order.SERVO_RELATIVE)
+            write_i8(self.serial_file, identifier)
+            write_i16(self.serial_file, int(message))
+            pub.sendMessage('log', msg="[serial] Servo(relative) " + str(identifier) + " " + str(message))
+            # print('[serial] Moved value from Arduino: ' + str(self.read16()))
+            pub.sendMessage('log', msg='[serial] Moved value from Arduino: ' + str(self.read16()))
+        elif type == serial.DEVICE_LED or type == 'led':
+            write_order(self.serial_file, Order.LED)
+            if isinstance(identifier, list) or isinstance(identifier, range):
+                # write the number of leds to update
+                write_i8(self.serial_file, len(identifier))
+                for i in identifier:
+                    write_i8(self.serial_file, i)
+            else:
+                write_i8(self.serial_file, 1)
+                write_i8(self.serial_file, identifier)
+
+            if isinstance(message, tuple):
+                for v in message:
+                    write_i8(self.serial_file, v)
+            else:
+                write_i16(self.serial_file, message)
+
+        elif type == serial.DEVICE_PIN or type == 'pin':
+            write_order(self.serial_file, Order.PIN)
+            write_i8(self.serial_file, identifier)
+            write_i8(self.serial_file, message)
+
+        elif type == serial.DEVICE_PIN_READ or type == 'pin_read':
+            pub.sendMessage('led', identifiers='status5', color='green')
+            write_order(self.serial_file, Order.READ)
+            write_i8(self.serial_file, identifier)
+            pub.sendMessage('led', identifiers='status5', color='off')
+            return read_i16(self.serial_file)
+        pub.sendMessage('led', identifiers='status5', color='off')
+        
+    from typing import Final
